41d838d78da43fc592c46bf406def88990266a94
Bardis <bardis@git.com>
2019-07-17 00:38:37 +0200

    error msg in registeration, use redirect instead of render

--- Camps/routes/index.js
+++ Camps/routes/index.js
@@ -23,7 +23,7 @@ router.post("/register", function(req, res){
     User.register(New_user, req.body.password, function(err, user){
         if(err){
             req.flash("error", err.message)
-            return res.render("register")
+            res.redirect("/register")
         }
 
         passport.authenticate("local")(req, res, function(){
